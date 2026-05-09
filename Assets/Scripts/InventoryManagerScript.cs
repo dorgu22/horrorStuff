90e5d1102520ac26c71cb1c75fda7e5a099f09bc
kanadetzz <kanadetzz@mail.ru>
2026-05-09 23:48:39 +0300

    Улучшил интерактив
    Добавил интерактив с дверьми, и подбирание ключей для них

--- Assets/Scripts/InventoryManagerScript.cs
+++ Assets/Scripts/InventoryManagerScript.cs
@@ -1,8 +1,11 @@
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class InventoryManagerScript : MonoBehaviour, IInteractable
 {
+    private static List<string> _inventoryObjects = new List<string>();
+
     void Start()
     {
 
@@ -17,18 +20,48 @@ public class InventoryManagerScript : MonoBehaviour, IInteractable
     {
         if (gameObject.tag == "Collectable")
         {
+            //switch(gameObject.name)
+            //{
+            //    case "TEST":
+            //        Debug.Log("1488");
+            //        break;
+            //    default:
+            //        break;
+            //}
+
+            gameObject.SetActive(false);
+            _inventoryObjects.Add(gameObject.name);
+        }
+        else if (gameObject.tag == "Interactable")
+        {
             switch(gameObject.name)
             {
-                case "TEST":
-                    Debug.Log("1488");
+                case "SecondKitchenDoorInteractable":
+                    DoorInteraction();
+                    break;
+                case "SecondHallwayDoorInteractable":
+                    DoorInteraction();
+                    break;
+                case "ShowerDoorInteractable":
+                    DoorInteraction();
                     break;
                 default:
                     break;
             }
         }
-        else if (gameObject.tag == "Interactable")
+    }
+
+    private void DoorInteraction()
+    {
+        if (_inventoryObjects.Contains($"{gameObject.name}Key"))
         {
-            
+            gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            _inventoryObjects.Remove($"{gameObject.name}Key");
+            Debug.Log($"{gameObject.name} открылась");
+        }
+        else
+        {
+            Debug.Log("нет ключа");
         }
     }
 }
